c43076c7d81f46ff3914761f680b02157fd4edaa
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-27 20:20:32 +0300

    Done practice cookies in APIs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
   
   def index
+    #byebug
     session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    cookies[:cookies_hello] ||= "Tim was here"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
